d93c3209e78a2db1bc8da086af0a4d90f04b4589
lauriap <lauri.poutanen@gmail.com>
2020-03-07 19:20:02 +0200

    jatkoa vko2

--- laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
@@ -1,41 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import maksukortti.maksukortti.Maksukortti;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- *
- * @author lauri
- */
 public class MaksukorttiTest {
-    
-    public MaksukorttiTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
+
+    Maksukortti kortti;
+
+    @Before
     public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+        kortti = new Maksukortti(10);
     }
 
-     @Test
-     public void hello() {}
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortinSaldoEiYlitaMaksimiarvoa() {
+        kortti.lataaRahaa(200);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
+    }
+    
+    /*
+    JATKA KOHDASTA 1!
+    
+    https://github.com/mluukkai/ohjelmistotekniikka-kevat-2020/blob/master/tehtavat/viikko2.md
+    
+    */
+    
+    
 }
