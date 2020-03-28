125f27f787d1777c269fe41a6da67b37ba569935
lauriap <lauri.poutanen@gmail.com>
2020-03-28 13:30:35 +0200

    Start of project

---
+++ MentalCalculation/src/main/java/gui/MentalCalculationGUI.java
@@ -0,0 +1,27 @@
+
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class MentalCalculationGUI extends Application {
+    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane menuPane = new Pane();
+        menuPane.setPrefSize(600, 400);
+  
+        Scene scene = new Scene(menuPane);
+        stage.setTitle("Mental Calculation Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
